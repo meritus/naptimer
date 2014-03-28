faa8bd70204395b0e1befb32fe9efcbfe973a0a9
omar Hashmi <hashmii786@live.com>
2014-03-28 14:20:12 -0400

    Initial Commit

---
+++ timer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  timer
+//
+//  Created by omar hashmi on 3/28/14.
+//  Copyright (c) 2014 AppPartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ timer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  timer
+//
+//  Created by omar hashmi on 3/28/14.
+//  Copyright (c) 2014 AppPartner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
