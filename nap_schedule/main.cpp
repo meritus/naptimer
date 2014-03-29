8eac92ce5139f42d85c8172560808d29167a93ab
Edison Liu <Liu997@nat-tvwna-inside-visitornet101-c-18204.princeton.org>
2014-03-29 14:17:31 -0400

    Initial Commit

---
+++ nap_schedule/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  nap_schedule
+//
+//  Created by Edison Liu on 3/29/14.
+//  Copyright (c) 2014 Edison Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
