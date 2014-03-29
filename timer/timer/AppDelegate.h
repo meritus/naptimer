6fb7a827ea00084b2405caa6bee2323261425d4b
omar Hashmi <hashmii786@live.com>
2014-03-28 22:03:34 -0400

    Initial

---
+++ timer/timer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  timer
+//
+//  Created by omar hashmi on 3/28/14.
+//  Copyright (c) 2014 AppPartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
